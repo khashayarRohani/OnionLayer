2464b23d7f9f9573ad4602991a3a7071f717f238
Khashayar <khashayarrhi@gmail.com>
2024-06-03 22:15:40 +0100

    adding update confirm,and view for updating

---
+++ MVCApplications/ViewModels/UpdateViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace MVCApplications.ViewModels
+{
+    public class UpdateViewModel
+    {
+        public string Name { get; set; }
+        public string Family { get; set; }
+        public int Grade { get; set; }
+    }
+}
